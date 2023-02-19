4ffb5f94b21d3e0a4f6c538f66394815e250121e
Deepak Sharma <deepak.sharma@idc.bedbath.com>
2023-02-19 19:28:53 +0530

    commit c1 in new_branch

--- DummyTesting/src/main/java/ApiVerification/ApiVerification.java
+++ DummyTesting/src/main/java/ApiVerification/ApiVerification.java
@@ -10,7 +10,7 @@ public class ApiVerification {
 
 	public static void responseCodeValiddation(Response response, int statusCode) {
 		Assert.assertEquals(statusCode, response.getStatusCode());
-		System.out.println("new branch commit message is different from the master branch message");
+		System.out.println("new branch commit c1");
 
 	}
 
