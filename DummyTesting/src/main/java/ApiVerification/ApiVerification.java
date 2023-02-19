d4c65e0150661f0a1aeb887050b0b22b9c124f06
Deepak Sharma <deepak.sharma@idc.bedbath.com>
2023-02-19 19:36:45 +0530

    commit in new branch

--- DummyTesting/src/main/java/ApiVerification/ApiVerification.java
+++ DummyTesting/src/main/java/ApiVerification/ApiVerification.java
@@ -10,7 +10,7 @@ public class ApiVerification {
 
 	public static void responseCodeValiddation(Response response, int statusCode) {
 		Assert.assertEquals(statusCode, response.getStatusCode());
-		System.out.println("new branch commit c1");
+		System.out.println("new branch commit c3");
 
 	}
 
