a9188377fd5e12aedd1e2ee75612f23d6b99058c
Deepak Sharma <deepaksh08@yahoo.com>
2023-04-01 13:57:06 +0530

    Merge Conflict file is added to the branch

---
+++ JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
@@ -0,0 +1,18 @@
+package ExcelPrograms;
+
+public class MergeConflict {
+	
+	public static void main(String[] args) {
+		
+		String a = null;
+		double b;
+		long c;
+		int d;
+		byte e;
+		char t;
+		float f;
+		
+		System.out.println("Hello world");
+	}
+
+}
