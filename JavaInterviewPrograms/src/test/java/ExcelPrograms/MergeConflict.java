8ee3aa4f235d594e9a840b41c0b3c8897fc096a7
Deepak Sharma <deepaksh08@yahoo.com>
2023-04-01 16:13:42 +0530

    1 april branch changes

--- JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
+++ JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
@@ -15,7 +15,7 @@ public class MergeConflict {
 
 		System.out.println("Hello ji branch 1 april wala commit + master wala commit wah ji wah git world");
 		System.out.println("this is new line added in origin");
-
+		System.out.println("this is new in  1 april branch");
 	}
 
 }
