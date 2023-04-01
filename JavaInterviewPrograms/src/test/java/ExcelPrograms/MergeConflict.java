7b69cb2c7e601d1035f3d956a03590b79f49305f
deepaksh08 <125257018+deepaksh08@users.noreply.github.com>
2023-04-01 18:09:43 +0530

    Update MergeConflict.java

--- JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
+++ JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
@@ -13,13 +13,8 @@ public class MergeConflict {
 		float f;
 		
 
-		System.out.println("Hello ji branch 1 april wala commit + master wala commit wah ji wah git world");
-		System.out.println("this is new line added in origin");
-
-		System.out.println("this is new in  1 april branch");
-
-		System.out.println("first line which is new");
-		System.out.println("second line which is new");
+		System.out.println("only line left with the main branch");
+		
 
 
 	}
