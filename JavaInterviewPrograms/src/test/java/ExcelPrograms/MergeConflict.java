d4b082ad0a0f9f2434a49d6043010400504172e6
Deepak Sharma <deepaksh08@yahoo.com>
2023-04-02 11:43:55 +0530

    commit 2nd april for revert testing

--- JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
+++ JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
@@ -3,7 +3,7 @@ package ExcelPrograms;
 public class MergeConflict {
 
 	public static void main(String[] args) {
-		
+
 		String a = null;
 		double b;
 		long c;
@@ -11,14 +11,10 @@ public class MergeConflict {
 		byte e;
 		char t;
 		float f;
-		
-
 
 		System.out.println("only line left with the main branch");
-        System.out.println("This is the only line added in 1april branch");
-
-		
-
+		System.out.println("This is the only line added in 1april branch");
+		System.out.println("Commit to revert 2 april");
 
 	}
 
