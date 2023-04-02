79d689232529f414b11ffbaad00116813fab5185
Deepak Sharma <deepaksh08@yahoo.com>
2023-04-02 11:56:52 +0530

    Revert "commit to revert"
    This reverts commit a98d1eb98c59cba505d137785f6699679ef88a7f.

--- JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
+++ JavaInterviewPrograms/src/test/java/ExcelPrograms/MergeConflict.java
@@ -16,8 +16,6 @@ public class MergeConflict {
 
 		System.out.println("only line left with the main branch");
         System.out.println("This is the only line added in 1april branch");
-        
-        System.out.println("this is the line which will be reverted");
 
 		
 
